5e0a1ae94e883121bfd082edd36caa12c5859d09
alessio ruggera <alessioruggera2@gmail.com>
2022-02-15 15:57:17 +0100

    no message

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 get_header(); ?>
 
 <?php if ( is_home() && ! is_front_page() && ! empty( single_post_title( '', false ) ) ) : ?>
-	<header class="page-header alignwide">
+	<header class="header">
 		<h1 class="page-title"><?php single_post_title(); ?></h1>
 	</header><!-- .page-header -->
 <?php endif; ?>
