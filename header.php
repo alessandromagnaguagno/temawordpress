e8429abe75cf0a7b7125cf751ccc0587167ebab2
Alessandro Magnaguagno <alessandro.magnaguagno@artigianelli.tn.it>
2022-02-22 14:29:16 +0100

    prova nome pagina title

--- header.php
+++ header.php
@@ -4,7 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php wp_title(); ?></title>
+   <title> 
+        <?php 
+            wp_title( '|', true, 'right' ); 
+            bloginfo('name');     
+            if ( is_home() || is_front_page() ) echo ' | ' . get_bloginfo( 'description' );
+        ?> 
+    </title>
     <?php wp_head(); ?>
 </head>
 <body>
