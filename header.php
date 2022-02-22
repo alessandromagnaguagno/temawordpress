4089a9e84795168f282ca8fc8f0a7e8b089002bc
Alessandro Magnaguagno <alessandro.magnaguagno@artigianelli.tn.it>
2022-02-22 12:00:15 +0100

    test

--- header.php
+++ header.php
@@ -6,7 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php wp_title(); ?></title>
     <?php wp_head(); ?>
+
+
 </head>
 <body>
 
-    <div> ... chiamare funzione wordpress che stampa il menù ... </div>
+    <div> ... chiamare funzione wordpress che stampa il menù ... 
+            <?php wp_nav_menu ([
+                'menu'            => 'menu principale',
+                 'container'       => 'nav',
+
+            ]) ?>
+       
+ 
+    </div>
